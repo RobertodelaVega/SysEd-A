9efc6d62f55563bb24347567281db7c956448481
Roberto de La Vega Z. <rdelavega@duoc.cl>
2025-07-10 13:00:39 -0400

    Carta Gantt

--- exportedModels.js
+++ exportedModels.js
@@ -1,4 +1,4 @@
-var JS_Array_Entries_Of_Models = new Array('m48b58de0-70bd-11dc-5e39-000ae4141842','mbba4b140-7631-11dc-5e39-000ae4141842','mb2647529-d7ef-4813-8b87-6db7f3de731e','m69840ca0-8e03-11dc-5e39-000ae4141842','mb882ee60-70cc-11dc-5e39-000ae4141842','m0c369590-8d31-11dc-5e39-000ae4141842','m935bc090-8d31-11dc-5e39-000ae4141842');function searchModel(model) {
+var JS_Array_Entries_Of_Models = new Array('m5d5a4ed0-5da2-11f0-24ea-5cb47e4f1a87','m7d05c850-242b-11f0-24ea-5cb47e4f1a87','madd2f4a0-5ccd-11f0-24ea-5cb47e4f1a87','m46fd31a0-42ce-11f0-24ea-5cb47e4f1a87','m83a24b40-5d9f-11f0-24ea-5cb47e4f1a87','m9c0045c0-5ccd-11f0-24ea-5cb47e4f1a87','m81224050-2429-11f0-24ea-5cb47e4f1a87');function searchModel(model) {
 for (var i = 0; i < JS_Array_Entries_Of_Models.length; i++) {
 if (model == JS_Array_Entries_Of_Models[i]) {
 return true;
